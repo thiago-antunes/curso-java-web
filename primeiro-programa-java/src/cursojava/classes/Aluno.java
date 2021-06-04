e01941e6a74b742bbdfdbd203e5909f010be9cbb
Thiago A. Ribeiro <Thiago A. Ribeiro@DESKTOP-HJOV6SL>
2021-06-04 17:18:58 -0300

    Criação da classe aluno e seus atributos

---
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,15 @@
+package cursojava.classes;
+
+/*Classe que representa cada aluno*/
+public class Aluno {
+
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+}
