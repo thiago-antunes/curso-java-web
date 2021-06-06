7050e5928610246ce2514a5e284a4b4770ec876d
Thiago A. Ribeiro <Thiago A. Ribeiro@DESKTOP-HJOV6SL>
2021-06-06 12:15:53 -0300

    Criação/Testes dos métodos Getters e Setters da classe Aluno - Aula 9.9

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -9,6 +9,7 @@ public class Aluno {
 	private String registroGeral;
 	private String numeroCpf;
 	private String nomeMae;
+	private String nomePai;
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
@@ -25,4 +26,88 @@ public class Aluno {
 		this.nome = nomePadrao;
 		this.idade = idadePadrao;
 	}
+	
+	/* Métodos Getters e Setters*/
+	/* SET é para adicionar ou receber dados para o atributo*/
+	/* GET é para resgatat ou obter o valor do atributo */
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
 }
--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -7,8 +7,29 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		
 		Aluno aluno1 = new Aluno(); // Aqui será o Joao
+		aluno1.setNome("João da Silva");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("18/10/1987");
+		aluno1.setRegistroGeral("111111111");
+		aluno1.setNumeroCpf("333.333.333-33");
+		aluno1.setNomeMae("Sandra");
+		aluno1.setNomePai("Junior");
+		aluno1.setDataMatricula("10/01/2019");
+		aluno1.setSerieMatriculado("5");
+		
+		System.out.println("O nome do aluno 1 é: " + aluno1.getNome());
+		System.out.println("O nome do aluno 1 é: " + aluno1.getIdade());
+		System.out.println("O nome do aluno 1 é: " + aluno1.getDataNascimento());
+		System.out.println("======================================================");
 		
 		Aluno aluno2 = new Aluno(); // Aqui será o Pedro
+		aluno2.setNome("Pedro");
+		aluno2.setIdade(40);
+		aluno2.setDataNascimento("10/10/1970");
+		
+		System.out.println("O nome do aluno 2 é: " + aluno2.getNome());
+		System.out.println("O nome do aluno 2 é: " + aluno2.getIdade());
+		System.out.println("O nome do aluno 2 é: " + aluno2.getDataNascimento());
 		
 		Aluno aluno3 = new Aluno(); // Aqui será o Alex
 		
