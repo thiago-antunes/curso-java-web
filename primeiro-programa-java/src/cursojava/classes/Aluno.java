2c6ebd9d4bd300f313e094a86335925229f7f50c
Thiago <Thiago@DESKTOP-VNB4CNU>
2021-08-21 09:53:07 -0300

    Criação das das propriedades de notas e método que calcula a média no modelo Aluno - Aula 9.11

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -13,22 +13,26 @@ public class Aluno {
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
-	
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+
 	public Aluno() {
 		// TODO Construtor padrão
 	}
-	
+
 	public Aluno(String nomePadrao) {
 		this.nome = nomePadrao;
 	}
-	
+
 	public Aluno(String nomePadrao, int idadePadrao) {
 		this.nome = nomePadrao;
 		this.idade = idadePadrao;
 	}
-	
-	/* Métodos Getters e Setters*/
-	/* SET é para adicionar ou receber dados para o atributo*/
+
+	/* Métodos Getters e Setters */
+	/* SET é para adicionar ou receber dados para o atributo */
 	/* GET é para resgatat ou obter o valor do atributo */
 	public String getNome() {
 		return nome;
@@ -109,5 +113,42 @@ public class Aluno {
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
 	
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+
+	/* Método que retorna a média do Aluno */
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
 }
--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -16,10 +16,15 @@ public class PrimeiraClasseJava {
 		aluno1.setNomePai("Junior");
 		aluno1.setDataMatricula("10/01/2019");
 		aluno1.setSerieMatriculado("5");
+		aluno1.setNota1(90);
+		aluno1.setNota2(80.8);
+		aluno1.setNota3(70.9);
+		aluno1.setNota4(90.7);
 		
 		System.out.println("O nome do aluno 1 é: " + aluno1.getNome());
 		System.out.println("O nome do aluno 1 é: " + aluno1.getIdade());
 		System.out.println("O nome do aluno 1 é: " + aluno1.getDataNascimento());
+		System.out.println("A média das notas é: " + aluno1.getMediaNota());
 		System.out.println("======================================================");
 		
 		Aluno aluno2 = new Aluno(); // Aqui será o Pedro
