ad1aef0eabfdcbaa45db4d320e26d8304ccb97d2
thiag <thiag@DESKTOP-JEMV2HT.mshome.net>
2021-08-22 05:21:54 -0400

    Criação do modelo Disciplina, relacionamento com o modelo Aluno e reafatoração - Aula 9.18

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -13,15 +13,8 @@ public class Aluno {
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
-	private double nota1;
-	private double nota2;
-	private double nota3;
-	private double nota4;
-	private String disciplina1;
-	private String disciplina2;
-	private String disciplina3;
-	private String disciplina4;
-
+	private Disciplina disciplina = new Disciplina();
+	
 	public Aluno() {
 		// TODO Construtor padrão
 	}
@@ -118,74 +111,17 @@ public class Aluno {
 		this.serieMatriculado = serieMatriculado;
 	}
 
-	public double getNota1() {
-		return nota1;
+	public Disciplina getDisciplina() {
+		return disciplina;
 	}
 
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
-
-	public String getDisciplina1() {
-		return disciplina1;
-	}
-
-	public void setDisciplina1(String disciplina1) {
-		this.disciplina1 = disciplina1;
-	}
-
-	public String getDisciplina2() {
-		return disciplina2;
-	}
-
-	public void setDisciplina2(String disciplina2) {
-		this.disciplina2 = disciplina2;
-	}
-
-	public String getDisciplina3() {
-		return disciplina3;
-	}
-
-	public void setDisciplina3(String disciplina3) {
-		this.disciplina3 = disciplina3;
-	}
-
-	public String getDisciplina4() {
-		return disciplina4;
-	}
-
-	public void setDisciplina4(String disciplina4) {
-		this.disciplina4 = disciplina4;
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
 	}
 
 	/* Método que retorna a média do Aluno */
 	public double getMediaNota() {
-		return (nota1 + nota2 + nota3 + nota4) / 4;
+		return (disciplina.getNota1() + disciplina.getNota2() + disciplina.getNota3() + disciplina.getNota4()) / 4;
 	}
 	
 	/* Método que retorna true para aprovado e false para reprovado */
@@ -198,19 +134,6 @@ public class Aluno {
 		}
 	}
 
-		
-
-	@Override
-	public String toString() {
-		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
-				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
-				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
-				+ serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4
-				+ ", disciplina1=" + disciplina1 + ", disciplina2=" + disciplina2 + ", disciplina3=" + disciplina3
-				+ ", disciplina4=" + disciplina4 + "]";
-	}
-
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -220,7 +143,6 @@ public class Aluno {
 		return result;
 	}
 	
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -243,8 +165,12 @@ public class Aluno {
 		return true;
 	}
 
-	
-	
-	
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
+				+ serieMatriculado + ", disciplina=" + disciplina + "]";
+	}
 	
 }
