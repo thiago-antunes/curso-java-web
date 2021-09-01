8bb395ab70c1842ea2751fb04cb2bbca2c67cfda
Thiago Antunes Ribeiro <thiago.antunes@msn.com>
2021-09-01 05:39:11 -0400

    Percorrendo listas pela posição - Aula 9.29

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -66,26 +66,17 @@ public class PrimeiraClasseJava {
 			alunos.add(aluno1);
 		}
 		
-		for (Aluno aluno : alunos) {
-			if (aluno.getNome().equalsIgnoreCase("Thiago")) {
-				alunos.remove(aluno);
-				break;
-			} else {
-				System.out.println(aluno);
-				System.out.println("Média do aluno: " + aluno.getMediaNota());
-				System.out.println("Resultado: " + (aluno.getAlunoAprovado2()));
-				System.out.println("----------------------------------------------------------------");
-			}
-		}
-
-		for (Aluno aluno : alunos) {
-			System.out.println("Alunos que sobraram na lista");
-			System.out.println(aluno.getNome());
-			System.out.println("Suas matérias são: ");
-			for (Disciplina disciplina : aluno.getDisciplinas()) {
-				System.out.println(disciplina.getDisciplina());
+		for (int pos = 0; pos < alunos.size(); pos++) {
+			Aluno aluno = alunos.get(pos);
+			System.out.println("Aluno: " + aluno.getNome());
+			System.out.println("Média do aluno: " + aluno.getMediaNota());
+			System.out.println("Resultado: " + (aluno.getAlunoAprovado2()));
+			System.out.println("----------------------------------------------------------------");
+			
+			for (int posd = 0; posd < aluno.getDisciplinas().size(); posd++) {
+				Disciplina disc = aluno.getDisciplinas().get(posd);
+				System.out.println("Matéria: " + disc.getDisciplina() + " -> Nota: " + disc.getNota());
 			}
 		}
 	}
-
 }
