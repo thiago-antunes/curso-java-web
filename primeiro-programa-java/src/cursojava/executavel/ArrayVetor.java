fbdaa051f983b9e02ec051ad236cb3eb7152f63e
Thiago Antunes Ribeiro <thiago.antunes@msn.com>
2021-09-09 04:40:31 -0400

    Fazendo entrada e leitura dinâmica em arrays - Aula 13.4

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -1,14 +1,18 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class ArrayVetor {
 
 	public static void main(String[] args) {
 
-		double notas[] = new double[5];
-		notas[0] = 7.8;
-		notas[1] = 8.7;
-		notas[2] = 9.7;
-		notas[3] = 9.9;
+		String posicoes = JOptionPane.showInputDialog("Quantas posições o array deve ter? ");
+		double[] notas = new double[Integer.parseInt(posicoes)];
+		
+		for (int pos = 0; pos < notas.length; pos++) {
+			String valor = JOptionPane.showInputDialog("Qual o valor da posição " + (pos + 1));
+			notas[pos] = Double.valueOf(valor);
+		}
 		
 		for (int pos = 0; pos < notas.length; pos++) {
 			System.out.println("Nota " + (pos + 1) + " = " + notas[pos]);
