4c192b4bc03beefdc2c27cfc95bec6725f3063be
Thiago Antunes Ribeiro <thiago.antunes@msn.com>
2022-06-24 15:19:12 -0400

    Teste de comunicação com GitHub

---
+++ primeiro-programa-java/src/cursojava/thread/ImplementacaoFilaThread.java
@@ -0,0 +1,22 @@
+package cursojava.thread;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ImplementacaoFilaThread extends Thread {
+
+	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilha_fila = new ConcurrentLinkedQueue<ObjetoFilaThread>();
+	
+	public static void add(ObjetoFilaThread objetoFilaThread) {
+		pilha_fila.add(objetoFilaThread);
+	}
+	
+	@Override
+	public void run() {
+		Iterator iteracao = pilha_fila.iterator();
+		while(iteracao.hasNext()) { /*Enquanto houver dados na lista irá processar*/
+			
+		}
+	}
+	
+}
--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -18,20 +18,20 @@ import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog {
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel jPanel = new JPanel(new GridBagLayout());
-	private JLabel descricaoHora= new JLabel("Time Thread 1");
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
 	private JTextField mostraTempo = new JTextField();
-	private JLabel descricaoHora2= new JLabel("Time Thread 2");
+	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
 	private JTextField mostraTempo2 = new JTextField();
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
-	
+
 	private Runnable thread1 = new Runnable() {
-		
+
 		@Override
 		public void run() {
-			while(true) {
+			while (true) {
 				mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
 				try {
 					Thread.sleep(1000);
@@ -41,69 +41,95 @@ public class TelaTimeThread extends JDialog {
 			}
 		}
 	};
-	
+
+	private Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+			while (true) {
+				mostraTempo2.setText(new SimpleDateFormat("dd-MM-yyyy hh:mm:ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+
 	private Thread thread1Time;
+	private Thread thread2Time;
 
 	/* Construtor da tela */
 	public TelaTimeThread() {
 		setTitle("Minha tela de time com Thread");
 		setSize(new Dimension(250, 250));
 		setLocationRelativeTo(null);
-		//setResizable(false);
+		// setResizable(false);
 		/* Parte inicial concluída */
-		
-		GridBagConstraints gridBagConstraints = new GridBagConstraints(); /*Controlador de posicionamento de componentes*/
+
+		GridBagConstraints gridBagConstraints = new GridBagConstraints(); /* Controlador de posicionamento de componentes */
 		gridBagConstraints.gridy = 0;
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridwidth = 2;
 		gridBagConstraints.insets = new Insets(5, 10, 5, 5);
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
-		
+
 		descricaoHora.setPreferredSize(new Dimension(200, 25));
 		jPanel.add(descricaoHora, gridBagConstraints);
-		
+
 		mostraTempo.setPreferredSize(new Dimension(200, 25));
-		gridBagConstraints.gridy ++;
+		gridBagConstraints.gridy++;
 		mostraTempo.setEditable(false);
 		jPanel.add(mostraTempo, gridBagConstraints);
-		
+
 		descricaoHora2.setPreferredSize(new Dimension(200, 25));
-		gridBagConstraints.gridy ++;
+		gridBagConstraints.gridy++;
 		mostraTempo2.setEditable(false);
 		jPanel.add(descricaoHora2, gridBagConstraints);
-		
+
 		mostraTempo2.setPreferredSize(new Dimension(200, 25));
-		gridBagConstraints.gridy ++;
+		gridBagConstraints.gridy++;
 		jPanel.add(mostraTempo2, gridBagConstraints);
-		
+
 		gridBagConstraints.gridwidth = 1;
-		
+
 		jButton.setPreferredSize(new Dimension(92, 25));
-		gridBagConstraints.gridy ++;
+		gridBagConstraints.gridy++;
 		jPanel.add(jButton, gridBagConstraints);
-		
+
 		jButton2.setPreferredSize(new Dimension(92, 25));
-		gridBagConstraints.gridx ++;
+		gridBagConstraints.gridx++;
 		jPanel.add(jButton2, gridBagConstraints);
-		
+
 		jButton.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				thread1Time = new Thread(thread1);
 				thread1Time.start();
+				
+				thread2Time = new Thread(thread2);
+				thread2Time.start();
+				
+				jButton.setEnabled(false);
+				jButton2.setEnabled(true);
 			}
 		});
-		
+
 		jButton2.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				thread1Time.stop();
+				thread2Time.stop();
+				jButton.setEnabled(true);
+				jButton2.setEnabled(false);
 			}
 		});
 		
-		
+		jButton2.setEnabled(false);
+
 		add(jPanel, BorderLayout.WEST);
 		setVisible(true); /* Exibe a tela para o usuário. Sempre deve ser o último a ser executado */
 	}
